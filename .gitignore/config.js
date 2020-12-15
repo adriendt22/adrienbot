49642ae6ed91fa8bf14b9b8943eb0ed32df70dbb
adriendt22 <55588625+adriendt22@users.noreply.github.com>
2020-12-15 21:34:44 +0100

    Add files via upload

---
+++ .gitignore/config.js
@@ -0,0 +1,7 @@
+exports.TOKEN = "NjIzMDk5NjcyNjcxODEzNjQy.XX9gyg.5Ls_yvvEMtwUgiIGfgUjX9l97_g";
+exports.PREFIX = "$";
+exports.HOST = "127.0.0.1";
+exports.PORT = "30120";
+exports.GAME = "fivem";
+exports.FOOTER = "Créer par $AdriXn DT#9999";
+exports.FOOTERI = "https://i.imgur.com/DZpPUem.png";
